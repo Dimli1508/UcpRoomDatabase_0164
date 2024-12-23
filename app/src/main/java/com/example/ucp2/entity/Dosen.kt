8b327333ee0f24daa66bfb3f085518b855c2958f
Mashell0707 <merlsutama@gmail.com>
2024-12-23 13:45:39 +0700

    membuat database untuk Dosen

--- app/src/main/java/com/example/ucp2/entity/Dosen.kt
+++ app/src/main/java/com/example/ucp2/entity/Dosen.kt
@@ -1,14 +1,9 @@
-package com.example.ucp2.entity
-
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "dosen")
 data class Dosen(
-    @PrimaryKey
-    val nidn: Int,
+    @PrimaryKey val nidn: String,
     val nama: String,
     val jenisKelamin: String
-
 )
