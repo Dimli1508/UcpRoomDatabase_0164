ce4ad2666eef8c3e2638ac5f773c96553d61caa4
Mashell0707 <merlsutama@gmail.com>
2024-12-23 13:46:23 +0700

    membuat database untuk MataKuliah

--- app/src/main/java/com/example/ucp2/entity/Mata Kuliah.kt
+++ app/src/main/java/com/example/ucp2/entity/Mata Kuliah.kt
@@ -1,17 +1,12 @@
-package com.example.ucp2.entity
-
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "mata_kuliah")
 data class MataKuliah(
-    @PrimaryKey
-    val kode: String,
+    @PrimaryKey val kode: String,
     val nama: String,
     val sks: Int,
     val semester: Int,
     val jenis: String,
     val dosenPengampu: String
-
 )
